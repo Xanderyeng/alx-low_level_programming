532d08a4ba58b54a939badd4a4783b8ceac956ca
xanderyeng <alexander.chepkiyeng@gmail.com>
2023-03-16 12:24:59 -0700

    using printf instead of puts and enforcing betty style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,18 @@
 #include<stdio.h>
 
-int main()
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints "with proper grammar,
+ * but the outcome is a piece of art," to the console
+ * using printf
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
 {
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
 
 	return (0);
 }
