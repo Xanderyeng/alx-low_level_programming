a684fbd11740c1353db86bdc6fb9a08b2f36b948
xanderyeng <alexander.chepkiyeng@gmail.com>
2023-03-16 13:14:52 -0700

    C program that prints exactly and that piece of art is useful

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints "and that piece of art is
+ * useful\" - Dora Korpar, 2015-10-19"
+ * to the console using printf / puts
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+	return (1);
+}
