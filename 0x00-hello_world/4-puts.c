b71a5995407152a8748d2586491b29b44e61f772
xanderyeng <alexander.chepkiyeng@gmail.com>
2023-03-16 12:11:18 -0700

    Using puts to print a line without using printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,16 @@
 #include<stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints "Programming is like building a multilingual puzzle" to the console
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	/* print a sentence */
+	/* print the sentence and adds a new line */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
