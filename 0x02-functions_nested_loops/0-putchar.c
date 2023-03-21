b6c079aa107f83cfedf8b9cf0607cbab3be758b1
xanderyeng <alexander.chepkiyeng@gmail.com>
2023-03-21 07:39:38 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,16 @@
 
 int main(void)
 {
-	char s[] = "_putchar";
+	char l[] = "_putchar";
 	int i = 0;
 
-	while (s[i] != '\0')
 	{
-		_putchar(s[i]);
+		_putchar(l[i]);
 		i++;
 	}
+
+	(l[i] != '\0')
+	
 	_putchar(10);
 
 	return (0);
