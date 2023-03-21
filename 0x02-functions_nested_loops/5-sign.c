c3591c966efc11a612629d172008eabe68e00ef5
xanderyeng <alexander.chepkiyeng@gmail.com>
2023-03-21 12:01:24 -0700

    printing based on the number being -ve, +ve or equal to zero

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * output_sign - Output the sign of a number
+ *	@n: zero, negative, positive digit
+ *	Return: 1 if > 0, 0 if equal to 0 and
+ *	-1 if less than zero
+ */
+
+int output_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
