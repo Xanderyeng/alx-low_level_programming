646a2905080903d7636db044576ea294d5729af4
xanderyeng <alexander.chepkiyeng@gmail.com>
2023-03-21 12:23:03 -0700

    multiplication table of 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * times_table - function that prints mutltiplication table for 9
+ *
+*/
+
+void times_table(void)
+{
+	int x = 0; /* factorial */
+	int y; /* count  */
+	int z; /* calculated value */
+
+	while (x < 10)
+	{
+		y = 0;
+		while (y < 10)
+		{
+			z = x * y;
+
+			if (z > 9)
+			{
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (y != 0)
+			{
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else
+			{
+				_putchar(z + '0');
+			}
+
+			if (y != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			y++;
+		}
+		_putchar('\n');
+		x++;
+	}
+}
