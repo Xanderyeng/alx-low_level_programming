d1736fc480975a499c40bd347c08d03cb3ae13b1
xanderyeng <alexander.chepkiyeng@gmail.com>
2023-03-28 18:42:56 -0700

    For loop failed. Had to switch to while loop

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse, followed by a new line.
+ *
+ * @s: The string to print in reverse.
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	while (len > 0)
+	{
+		_putchar(s[--len]);
+	}
+
+	 _putchar('\n');
+}
