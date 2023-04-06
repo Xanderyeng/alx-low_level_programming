33c681680ef45c8edbd43085655930a05db854b3
xanderyeng <alexander.chepkiyeng@gmail.com>
2023-04-05 19:46:40 -0700

    function that returns 1 if the input integer is a prime number, otherwise return 0

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,30 +1,30 @@
 /**
- * _sqrt_recursion - returns the natural square root of a number
+ * _sqrt_recurs - returns the natural square root of a number
  * @n: the number to compute the square root of
+ * @y: number to evaluate / consider
  *
  * Return: the square root of n, or -1 if n does not have a natural square root
  */
-int _sqrt_recursion(int n)
+int _sqrt_recurs(int n, int y)
 {
-	if (n < 0)
-	return (-1);
-	if (n == 0 || n == 1)
-	return (n);
-	return (_sqrt_helper(n, 1));
+	if (n < (y * y))
+	{
+		return (-1);
+	}
+	else if (n == y * y)
+		return (y);
+	else
+		return (_sqrt_aux(n, y + 1));
 }
 
 /**
- * _sqrt_helper - recursive helper function for _sqrt_recursion
- * @n: the number to compute the square root of
- * @i: the current guess for the square root of n
- *
- * Return: the square root of n, or -1 if n does not have a natural square root
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: number to evaluate
+ * Return: y
  */
-int _sqrt_helper(int n, int i)
+
+int _sqrt_recursion(int n)
 {
-	if (i * i == n)
-	return (i);
-	if (i * i > n)
-	return (-1);
-	return (_sqrt_helper(n, i + 1));
+
+	return (_sqrt_aux(n, 1));
 }
