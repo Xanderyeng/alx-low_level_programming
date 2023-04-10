9bf0cce58c605469e8ea48d16fcbb2828568fa1c
xanderyeng <alexander.chepkiyeng@gmail.com>
2023-04-10 12:03:50 -0700

    a program that prints all arguments it receives

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,32 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * main - multiplies a list of numbers provided as command-line arguments
- * @argc: the number of command-line arguments
- * @argv: an array of strings containing the command-line arguments
- *
- * Return: 0 if successful, 1 if an error occurred
+ * main - multiplication of numbers
+ * @argc: argc
+ * @argv: argv
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	int i, mult;
+	int i;
+	int mult;
 
 	if (argc > 2)
 	{
 		mult = 1;
-
 		for (i = 1; i < argc; i++)
 		{
-			mult *= atoi(argv[i]);
+			mult = mult * atoi(argv[i]);
 		}
-			printf("%d\n", mult);
-			return (0);
-		}
+		printf("%d\n", mult);
+		return (0);
+	}
 	else
 	{
 		printf("Error\n");
-		return (1);
 	}
+	return (1);
 }
-
