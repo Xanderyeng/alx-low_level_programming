7007b5df01073fc2b2afda13fe123ea2ddc03175
xanderyeng <alexander.chepkiyeng@gmail.com>
2023-04-13 12:52:39 -0700

    a function that allocates memory for an array, using malloc.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,42 +1,50 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * string_nconcat - concatenates two strings
- * @s1: the first string
- * @s2: the second string
- * @n: the maximum number of bytes of s2 to concatenate
- *
- * Return: a pointer to the concatenated string
- *         or NULL if the function fails
+ * string_nconcat - concatenate tow strings
+ * @s1: string one
+ * @s2: string tow
+ * @n: number
+ * Return: NULL or p
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *result;
-	unsigned int s1_len = 0, s2_len = 0, i;
+	char *p;
+	unsigned int i = 0, leng1 = 0, j = 0, k = 0, leng2 = 0;
 
 	if (s1 == NULL)
 		s1 = "";
+	while (s1[i] != '\0')
+		leng1++, i++;
 	if (s2 == NULL)
 		s2 = "";
-	while (s1[s1_len] != '\0')
-		s1_len++;
-	while (s2[s2_len] != '\0')
-		s2_len++;
-
-	if (n >= s2_len)
-		n = s2_len;
-
-	result = malloc_checked((s1_len + n + 1) * sizeof(char));
-	if (result == NULL)
-		return (NULL);
-
-	for (i = 0; i < s1_len; i++)
-		result[i] = s1[i];
-	for (i = 0; i < n; i++)
-		result[s1_len + i] = s2[i];
-	result[s1_len + n] = '\0';
-
-	return (result);
+	i = 0;
+	while (s2[i] != '\0')
+	{
+		leng2++;
+		i++;
+	}
+	if (n >= leng2)
+		n = leng2;
+	p = malloc(sizeof(char) * (leng1 + n + 1));
+	if (p != NULL)
+	{
+		while (j < leng1)
+		{
+			p[j] = s1[j];
+			j++;
+		}
+		while (k < n)
+		{
+			p[j] = s2[k];
+			k++;
+			j++;
+		}
+	p[j] = '\0';
+	return (p);
+	}
+	return (NULL);
 }
