b6a5540c666ddaa2e4899626f2967825e7b75101
xanderyeng <alexander.chepkiyeng@gmail.com>
2023-04-13 13:50:59 -0700

    function that reallocates a memory block using malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,82 +10,39 @@
  *
  * Return: pointer to the reallocated memory block, or NULL if fails
  */
+
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *new_ptr, *old_ptr;
+	char *s;
 	unsigned int i;
+	char *ptp;
 
+	ptp = ptr;
 	if (new_size == old_size)
 		return (ptr);
-
-	if (!ptr)
+	if (ptr == NULL)
 	{
-		new_ptr = malloc(new_size);
-		if (!new_ptr)
+		ptr = malloc(new_size);
+		if (ptr == NULL)
 			return (NULL);
-		return (new_ptr);
+		return (ptr);
 	}
-
-	if (!new_size)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-
-	new_ptr = malloc(new_size);
-	if (!new_ptr)
-		return (NULL);
-
-	old_ptr = ptr;
-	for (i = 0; i < old_size && i < new_size; i++)
-		new_ptr[i] = old_ptr[i];
-
-	free(ptr);
-
-	return (new_ptr);
-}
-#include "main.h"
-#include <stdlib.h>
-
-/**
- * _realloc - reallocates a memory block using malloc and free
- * @ptr: pointer to the memory previously allocated with malloc
- * @old_size: size, in bytes, of the allocated space for ptr
- * @new_size: new size, in bytes, of the new memory block
- *
- * Return: pointer to the reallocated memory block, or NULL if fails
- */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
-{
-	char *new_ptr, *old_ptr;
-	unsigned int i;
-
-	if (new_size == old_size)
-		return (ptr);
-
-	if (!ptr)
+	if (new_size > old_size)
 	{
-		new_ptr = malloc(new_size);
-		if (!new_ptr)
+		s = malloc(new_size);
+		if (s == NULL)
+		{
+			free(s);
 			return (NULL);
-		return (new_ptr);
-	}
-
-	if (!new_size)
-	{
+		}
+		for (i = 0; i < old_size; i++)
+			s[i] = ptp[i];
 		free(ptr);
-		return (NULL);
 	}
-
-	new_ptr = malloc(new_size);
-	if (!new_ptr)
-		return (NULL);
-
-	old_ptr = ptr;
-	for (i = 0; i < old_size && i < new_size; i++)
-		new_ptr[i] = old_ptr[i];
-
-	free(ptr);
-
-	return (new_ptr);
+	return (s);
 }
