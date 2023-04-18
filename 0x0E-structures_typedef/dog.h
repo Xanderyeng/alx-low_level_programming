2444c49b508ff249e3df2a968006bd0e2a071901
xanderyeng <alexander.chepkiyeng@gmail.com>
2023-04-17 19:43:37 -0700

    structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,18 @@
-struct dog {
-   char *name;
-   float age;
-   char *owner;
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - my dear friend rex
+ *
+ * @name: 1st member
+ * @owner: 2nd member
+ * @age: 3rd member
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
