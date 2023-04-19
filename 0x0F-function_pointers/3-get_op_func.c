2bd4859208aeb26b2ea1a0e072f2c8331f8481d3
xanderyeng <alexander.chepkiyeng@gmail.com>
2023-04-19 12:57:47 -0700

    a program that performs simple operations.

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * get_op_func - Selects correct function
+ *	to perform the operation
+ *	asked by the user.
+ * @s: The operator passed as argument to the program.
+ *
+ * Return: pointer to the function that matches the
+ *	operator given as a parameter.
+ *	If s does not match any of the 5
+ *	expected operators (+, -, *, /, %),
+ *	return NULL.
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (*(ops[i].op) == *s)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * main - Entry point
+ * @argc: number of arguments passed to the program
+ * @argv: An array containing arguments passed to the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *
