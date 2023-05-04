2980ade8965f0210ddff7e33e2898dea9514c283
xanderyeng <alexander.chepkiyeng@gmail.com>
2023-05-04 08:17:31 -0700

    a function that sets the value of a bit to 1 at a given index.

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,28 +1,17 @@
 #include "main.h"
 
 /**
- * print_binary - Prints the binary representation of a number
- * @n: The number to be converted
- *
- * Return: void
+ * print_binary - prints binary representatiom of a number
+ * @n: number
  */
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = 1;
-	int bit;
-
-	/* Move the mask to the left until the leftmost bit is 1 */
-	while ((mask << 1) > 0)
+	if (n <= 1)
 	{
-		mask <<= 1;
+		_putchar(n + '0');
+		return;
 	}
-
-	/* Print the binary representation of the number */
-	while (mask > 0)
-	{
-		bit = (n & mask) ? 1 : 0;
-		_putchar(bit + '0');
-		mask >>= 1;
-	}
+	print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
