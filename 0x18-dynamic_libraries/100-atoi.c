a9e421ede62f8f175e1f16df993fdd3e3ae4c224
xanderyeng <alexander.chepkiyeng@gmail.com>
2023-06-19 15:33:28 -0700

    dynamic library libdynamic.so containing all the functions listed

---
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _atoi - Converts a string to an integer.
+ *
+ * @s: The string to convert.
+ *
+ * Return: The integer value of the string.
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+	int i = 0;
+
+    /* Handle leading whitespace */
+	while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n')
+	{
+		i++;
+	}
+
+	/* Handle sign */
+	if (s[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (s[i] == '+')
+	{
+		i++;
+	}
+
+	/* Handle digits */
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
+
+	return (sign * result);
+}
+
---
+++ 0x18-dynamic_libraries/_strstr.c
@@ -0,0 +1,13 @@
+/**
+ * _strstr - Locates a substring.
+ * @haystack: The main string to be searched.
+ * @needle: The substring to be found.
+ *
+ * Return: A pointer to start of substring, or
+ *         NULL if the substring is not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	/* TODO: Implement the _strstr function */
+	return NULL;
+}
